891579838c052976799607a4bdb7542d217d757a
27diego <divegamaravilla@csumb.edu>
2020-02-15 00:06:08 -0800

    moving on to animations

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./Navbar.scss";
 import sunny from "../../images/Icons/wb_sunny-24px.svg";
+import dark from "../../images/Icons/nights_stay-24px.svg";
 
 export const Navbar = () => {
   const [show, setShow] = useState("show");
+  const [theme, setTheme] = useState("dark");
 
   return (
     <div className={`navContainer navContainer--${show}`}>
@@ -22,8 +24,15 @@ export const Navbar = () => {
         className={`navbtn navbtn--${show}`}
       ></button>
 
-      <button className="themebtn">
-        <img className="img img--sunny" src={sunny} alt="sun" />
+      <button
+        onClick={() => setTheme(theme === "dark" ? "sunny" : "dark")}
+        className={`themebtn themebtn--${theme}`}
+      >
+        <img
+          className={`img img--${theme}`}
+          src={theme === "dark" ? dark : sunny}
+          alt="sun"
+        />
       </button>
     </div>
   );

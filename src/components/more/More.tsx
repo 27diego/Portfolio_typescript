df2528ffa7cfc4b983c550ee6efcdc6e44cb82d0
27diego <divegamaravilla@csumb.edu>
2020-05-04 20:08:19 -0700

    almost done with about me page

--- src/components/more/More.tsx
+++ src/components/more/More.tsx
@@ -1,83 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./More.scss";
 
 import portrait from "../../images/portrait.png";
 
 import Particles, { HoverMode } from "react-particles-js";
+import { Why } from "./Content";
+import { Background } from "./Content";
+import { Future } from "./Content";
+import { Hobby } from "./Content";
 
-export const More: React.FC<NavBarProps> = ({ main, setMain }) => {
-  const [activate, setActivate] = useState<boolean>(false);
-  const [backArrow, setBackArrow] = useState<string>("");
+interface Props {
+  main: boolean;
+  setMain: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-  useEffect(() => {
+interface State {
+  activate: boolean;
+  backArrow: string;
+  component: number;
+}
+
+export class More extends React.Component<Props, State> {
+  state = {
+    activate: false,
+    backArrow: "",
+    component: 0,
+  };
+
+  componentDidMount() {
     setTimeout(() => {
-      setActivate(true);
+      this.setState({ activate: true });
     }, 1500);
-  }, [activate]);
+  }
 
-  return (
+  increment = () => {
+    if (this.state.component === 4) {
+      this.setState({ component: 0 });
+    } else {
+      this.setState((prevState) => ({ component: prevState.component + 1 }));
+    }
+  };
+  decrement = () => {
+    if (this.state.component === 0) {
+      this.setState({ component: 4 });
+    } else {
+      this.setState((prevState) => ({ component: prevState.component - 1 }));
+    }
+  };
+
+  first = () => (
     <React.Fragment>
-      <div className={`controls controls--${activate ? "show" : "hide"}`}>
-        <div className="controls__container controls__container--left">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            className="controls--left"
-          >
-            <polyline
-              points="244 400 100 256 244 112"
-              style={{
-                fill: "none",
-                strokeLinecap: "round",
-                strokeLinejoin: "round",
-                strokeWidth: "48px",
-              }}
-            />
-            <line
-              x1="120"
-              y1="256"
-              x2="412"
-              y2="256"
-              style={{
-                fill: "none",
-                strokeLinecap: "round",
-                strokeLinejoin: "round",
-                strokeWidth: "48px",
-              }}
-            />
-          </svg>
-        </div>
-        <div className="controls__container controls__container--right">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            className="controls--right"
-          >
-            <polyline
-              points="244 400 100 256 244 112"
-              style={{
-                fill: "none",
-                strokeLinecap: "round",
-                strokeLinejoin: "round",
-                strokeWidth: "48px",
-              }}
-            />
-            <line
-              x1="120"
-              y1="256"
-              x2="412"
-              y2="256"
-              style={{
-                fill: "none",
-                strokeLinecap: "round",
-                strokeLinejoin: "round",
-                strokeWidth: "48px",
-              }}
-            />
-          </svg>
-        </div>
-      </div>
-
       <Particles
         params={{
           particles: {
@@ -108,17 +80,19 @@ export const More: React.FC<NavBarProps> = ({ main, setMain }) => {
       />
       <div className="Container--More">
         <div className="main">
-          <div className={`home home--${activate ? "show" : "hide"}`}>
+          <div
+            className={`home home--${this.state.activate ? "show" : "hide"}`}
+          >
             <div
               className="homeArrow__container"
-              onMouseOver={(): void => setBackArrow("back")}
-              onMouseOut={(): void => setBackArrow("")}
-              onClick={(): void => setMain(true)}
+              onMouseOver={(): void => this.setState({ backArrow: "back" })}
+              onMouseOut={(): void => this.setState({ backArrow: "" })}
+              onClick={(): void => this.props.setMain(true)}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
-                className={`homeArrow homeArrow--${backArrow}`}
+                className={`homeArrow homeArrow--${this.state.backArrow}`}
               >
                 <polyline
                   points="244 400 100 256 244 112"
@@ -147,14 +121,16 @@ export const More: React.FC<NavBarProps> = ({ main, setMain }) => {
           </div>
           <div
             className={`name__container name__container--${
-              activate ? "load" : "initial"
+              this.state.activate ? "load" : "initial"
             }`}
           >
             <h1 className="name">Diego Vega</h1>
           </div>
 
           <img
-            className={`portrait portrait--${activate ? "show" : "hide"}`}
+            className={`portrait portrait--${
+              this.state.activate ? "show" : "hide"
+            }`}
             src={portrait}
             alt="self portrait"
           />
@@ -162,4 +138,98 @@ export const More: React.FC<NavBarProps> = ({ main, setMain }) => {
       </div>
     </React.Fragment>
   );
-};
+
+  renderComponents = () => {
+    switch (this.state.component) {
+      case 0:
+        return this.first();
+      case 1:
+        return <Why />;
+      case 2:
+        return <Background />;
+      case 3:
+        return <Hobby />;
+      case 4:
+        return <Future />;
+    }
+  };
+
+  render() {
+    console.log(this.state.component);
+    return (
+      <React.Fragment>
+        <div
+          className={`controls controls--${
+            this.state.activate ? "show" : "hide"
+          }`}
+        >
+          <div
+            onClick={this.decrement}
+            className="controls__container controls__container--left"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="controls--left"
+            >
+              <polyline
+                points="244 400 100 256 244 112"
+                style={{
+                  fill: "none",
+                  strokeLinecap: "round",
+                  strokeLinejoin: "round",
+                  strokeWidth: "48px",
+                }}
+              />
+              <line
+                x1="120"
+                y1="256"
+                x2="412"
+                y2="256"
+                style={{
+                  fill: "none",
+                  strokeLinecap: "round",
+                  strokeLinejoin: "round",
+                  strokeWidth: "48px",
+                }}
+              />
+            </svg>
+          </div>
+          <div
+            onClick={this.increment}
+            className="controls__container controls__container--right"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="controls--right"
+            >
+              <polyline
+                points="244 400 100 256 244 112"
+                style={{
+                  fill: "none",
+                  strokeLinecap: "round",
+                  strokeLinejoin: "round",
+                  strokeWidth: "48px",
+                }}
+              />
+              <line
+                x1="120"
+                y1="256"
+                x2="412"
+                y2="256"
+                style={{
+                  fill: "none",
+                  strokeLinecap: "round",
+                  strokeLinejoin: "round",
+                  strokeWidth: "48px",
+                }}
+              />
+            </svg>
+          </div>
+        </div>
+        {this.renderComponents()}
+      </React.Fragment>
+    );
+  }
+}

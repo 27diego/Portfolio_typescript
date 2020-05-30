90a920eeedcd162ffeb288285c3eb7a304d86563
27diego <divegamaravilla@csumb.edu>
2020-05-30 13:59:51 -0700

    some little fixes

--- src/components/outro/Outro.tsx
+++ src/components/outro/Outro.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import { ThemeContext } from '../../context/ThemeContext';
 import './Outro.scss';
 
@@ -13,6 +13,15 @@ export const Outro: React.FC<OutroProps> = ({ setMain }) => {
     email: '',
     message: '',
   });
+
+  const [more, setMore] = useState(true);
+
+  const container = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (container.current!.getBoundingClientRect().width <= 500) {
+      setMore(false);
+    }
+  }, []);
 
   const context = useContext(ThemeContext);
   const { theme } = context;
@@ -43,7 +52,10 @@ export const Outro: React.FC<OutroProps> = ({ setMain }) => {
   };
 
   return (
-    <div className={`Container--Outro Container--Outro--${theme}`}>
+    <div
+      ref={container}
+      className={`Container--Outro Container--Outro--${theme}`}
+    >
       <div className='container--form'>
         <form className={`form form--${theme}`}>
           <div className={`form__header form__header--${theme}`}>
@@ -127,7 +139,7 @@ export const Outro: React.FC<OutroProps> = ({ setMain }) => {
           >
             <img
               src={github}
-              className='links__icon links__icon--github'
+              className={`links__icon links__icon--github links__icon--github--${theme}`}
               alt='github logo'
             />
           </a>
@@ -136,7 +148,19 @@ export const Outro: React.FC<OutroProps> = ({ setMain }) => {
           className={`links__icon--container links__icon--container--${theme} links__icon--my`}
         >
           <div onClick={() => setMain(false)} className='links__icon--my'>
-            More about me
+            {more ? (
+              'More about me'
+            ) : (
+              <svg
+                className={`moreIcon moreIcon--${theme}`}
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 512 512'
+              >
+                <title>ionicons-v5-j</title>
+                <path d='M258.9,48C141.92,46.42,46.42,141.92,48,258.9,49.56,371.09,140.91,462.44,253.1,464c117,1.6,212.48-93.9,210.88-210.88C462.44,140.91,371.09,49.56,258.9,48ZM385.32,375.25a4,4,0,0,1-6.14-.32,124.27,124.27,0,0,0-32.35-29.59C321.37,329,289.11,320,256,320s-65.37,9-90.83,25.34a124.24,124.24,0,0,0-32.35,29.58,4,4,0,0,1-6.14.32A175.32,175.32,0,0,1,80,259C78.37,161.69,158.22,80.24,255.57,80S432,158.81,432,256A175.32,175.32,0,0,1,385.32,375.25Z' />
+                <path d='M256,144c-19.72,0-37.55,7.39-50.22,20.82s-19,32-17.57,51.93C191.11,256,221.52,288,256,288s64.83-32,67.79-71.24c1.48-19.74-4.8-38.14-17.68-51.82C293.39,151.44,275.59,144,256,144Z' />
+              </svg>
+            )}
           </div>
         </div>
         <div className={`links links--${theme} links--2`}>Or View My Work</div>
